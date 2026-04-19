0fae100245be9eaf1c8d1ae5d2229a4e9f28b935
schundi1405 <schundi1405@gmail.com>
2026-04-19 19:40:58 -0400

    Initial Commit

---
+++ Letterbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Letterbox
+//
+//  Created by Sindhu's iPad on 4/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Letterbox/LetterboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterboxApp.swift
+//  Letterbox
+//
+//  Created by Sindhu's iPad on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct LetterboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
